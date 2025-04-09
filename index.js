104ae6564c5a89cc1d04e909ed71b03b05d4d290
Stone Holt <stoneholt22@gmail.com>
2025-04-08 22:30:56 -0400

    up to 6.3.1.2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import session from "express-session";
 import express from 'express'
+import mongoose from "mongoose";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -9,6 +10,8 @@ import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
     credentials: true,
@@ -37,3 +40,5 @@ EnrollmentsRoutes(app);
 Lab5(app);
 Hello(app)
 app.listen(process.env.PORT || 4000)
+
+// mongodb username: asdf, password: asdf
